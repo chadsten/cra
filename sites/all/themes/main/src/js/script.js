c58ba135883c8c69d130235ccf2add28611cf95a
Johnny Martin <johnny@johnny5th.com>
2014-08-09 13:36:53 -0500

    Header done

--- sites/all/themes/main/src/js/script.js
+++ sites/all/themes/main/src/js/script.js
@@ -14,66 +14,10 @@
 
 	$(function() { // Ready
 
-    $('.menu-button').click(function() {
-      $('.primary-navigation').toggleClass('open');
+    $('#header-inner .mobile-icon').click(function() {
+      $('#block-system-main-menu').toggleClass('open');
       return false;
     });
-
-    // Label hiding
-    var form_elements = $('.form-type-textfield, .webform-component-textfield, .webform-component-textfield, .webform-component-textarea, .form-email, .form-type-password, .webform-component-email');
-
-    form_elements.find('input, textarea').change(function() {
-      var elem = $(this);
-      if(elem.val() != "")
-        $('label[for="'+elem.attr('id')+'"]').hide();
-    });
-
-    form_elements.find('input, textarea').focus(function() {
-      var elem = $(this);
-      $('label[for="'+elem.attr('id')+'"]').hide();
-    }).blur(function() {
-      var elem = $(this);
-      if(elem.val() == "")
-        $('label[for="'+elem.attr('id')+'"]').show();
-    });
-    // End Label Hiding
-
-    // Search/Filter
-    $('.search-filter .search').click(function() {
-      $(this).toggleClass('open');
-      $('.dropdown-search').toggleClass('open');
-      $('.search-filter .filter').removeClass('open');
-      $('.dropdown-filter').removeClass('open');
-    });
-
-    $('.search-filter .filter').click(function() {
-      $(this).toggleClass('open');
-      $('.dropdown-filter').toggleClass('open');
-      $('.search-filter .search').removeClass('open');
-      $('.dropdown-search').removeClass('open');
-      $('.dropdown-filter .title').removeClass('open').siblings('ul').removeClass('open');
-    });
-
-    $('.dropdown-filter .title').click(function() {
-      $(this).toggleClass('open').siblings('ul').toggleClass('open');
-      $('.dropdown-filter .title').not(this).removeClass('open').siblings('ul').removeClass('open');
-    });
-    // End Search/Filter
-
-    // Accordions
-    if ($('.view-id-engineered_systems_detail').length > 0) {
-      var allPanels = $('.view-id-engineered_systems_detail .views-row > .content').hide();
-      var allHeadings = $('.view-id-engineered_systems_detail .views-row > .heading');
-
-      $('.view-id-engineered_systems_detail .views-row > .heading').click(function() {
-        allHeadings.removeClass('active');
-        allPanels.slideUp('fast');
-        $(this).next().slideDown('fast');
-        $(this).addClass('active');
-        return false;
-      });
-    }
-    // End Accordions
 
 	}); // End Ready
 
