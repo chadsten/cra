2b107f82b802ec6bee94259a670e2768105b8c55
Chadsten Lowery <chadsten.lowery@gmail.com>
2014-08-10 13:18:26 -0500

    contact page and client login button

--- sites/all/themes/main/src/js/script.js
+++ sites/all/themes/main/src/js/script.js
@@ -19,6 +19,21 @@
       return false;
     });
 
+    // Accordions
+    if ($('.view-id-engineered_systems_detail').length > 0) {
+      var allPanels = $('.view-id-engineered_systems_detail .views-row > .content').hide();
+      var allHeadings = $('.view-id-engineered_systems_detail .views-row > .heading');
+
+      $('.view-id-engineered_systems_detail .views-row > .heading').click(function() {
+        allHeadings.removeClass('active');
+        allPanels.slideUp('fast');
+        $(this).next().slideDown('fast');
+        $(this).addClass('active');
+        return false;
+      });
+    }
+    // End Accordions
+
 	}); // End Ready
 
 })(jQuery, Drupal, this, this.document);
